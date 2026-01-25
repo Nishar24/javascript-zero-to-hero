6b46b823bc5f04113b6dd19f9951f2c744eadc1b
Nishar <mdnishar75640@gmail.com>
2026-01-26 03:33:30 +0530

    Add files via upload

---
+++ 4. Hof.js/05_Remove_Number.js
@@ -0,0 +1,14 @@
+function destroyer(arr) {
+  const valuesToRemove = Array.from(arguments).slice(1);
+
+  return arr.filter(item => !valuesToRemove.includes(item));
+}
+
+console.log(destroyer([1, 2, 3, 1, 2, 3], 2, 3));
+// [1, 1]
+
+console.log(destroyer(["tree", "hamburger", 53], "tree", 53));
+// ["hamburger"]
+
+console.log(destroyer([2, 3, 2, 3], 2, 3));
+// []
