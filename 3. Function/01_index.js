239cc6f6a3f2b2084a22b726866333f32d1a8d7f
Nishar <mdnishar75640@gmail.com>
2026-02-01 23:09:10 +0530

    Uncomment recipe and utility function code

--- 3. Function/01_index.js
+++ 3. Function/01_index.js
@@ -1,7 +1,7 @@
 const recipes = [];
 
  const recipe1 = {
-//   name: "Spaghetti Carbonara",
+   name: "Spaghetti Carbonara",
 //   ingredients: ["spaghetti", "Parmesan cheese", "pancetta", "black pepper"],
 //   cookingTime: 22,
 //   totalIngredients: null,
@@ -304,6 +304,7 @@ let obj = {
 
 let result = obj.increment().double().increment().getValue();
 console.log(result);
+
 
 
 
