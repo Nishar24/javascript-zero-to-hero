108d3bf3314e411f27d9c28e68f0333f4dfb1caf
Nishar <mdnishar75640@gmail.com>
2026-02-01 23:01:38 +0530

    Refactor diffArray function with updated comments

--- 4. Hof.js/04_Diffence_function.js
+++ 4. Hof.js/04_Diffence_function.js
@@ -2,7 +2,7 @@ function diffArray(arr1, arr2) {
   // arr1 se wo element jo arr2 me nhi h
   const onlyInArr1 = arr1.filter(item => !arr2.includes(item));
 
-  // arr2 से वो elements जो arr1 में नहीं हैं
+  // arr2 se wo element jo arr1 me nhi he
   const onlyInArr2 = arr2.filter(item => !arr1.includes(item));
 
   // दोनों को जोड़ दो
@@ -12,4 +12,5 @@ function diffArray(arr1, arr2) {
 console.log(
   diffArray(["diamond", "stick", "apple"], ["stick", "emerald", "bread"])
 );
+
 
