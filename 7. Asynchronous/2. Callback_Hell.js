0bd0f6da421311a50de2716d8909d4c23c7978d6
Nishar <mdnishar75640@gmail.com>
2026-02-13 22:33:43 +0530

    Add files via upload

---
+++ 7. Asynchronous/2. Callback_Hell.js
@@ -0,0 +1,19 @@
+// 📖 Kahani
+
+// Socho:
+
+// Pehle chai
+
+// Phir biscuit
+
+// Phir phone call
+// Sab ek ke baad ek 😖
+
+makeChai(() =>{
+    getBiscuit(() =>{
+        callFriend(() =>{
+            console.log("Sab kaam ho gaye");
+            
+        })
+    })
+})
