858e1e14025df0fe035d5c7662ef9dfd9c46d198
Nishar <mdnishar75640@gmail.com>
2026-02-14 23:48:33 +0530

    Add files via upload

---
+++ 7. Asynchronous/3.3. OTP_Promise.js
@@ -0,0 +1,28 @@
+// 📱 Real Life Example 3: OTP Verification
+// 📖 Kahani
+
+// OTP bheja gaya 📩
+
+// OTP sahi → Login success (resolve)
+
+// OTP galat → Access denied (reject)
+
+function verifyOTP(otp){
+    return new Promise((resolve, reject) =>{
+        console.log("🔐 OTP verify ho raha hai...");
+
+        setTimeout(() =>{
+
+            if(otp === 1234){
+                resolve("✅ Login successful")
+            }else{
+                reject("❌ Invalid OTP");
+            }
+        }, 1000)
+        
+    });
+}
+
+verifyOTP(1234)
+  .then(msg => console.log(msg))
+  .catch(err => console.log(err))
