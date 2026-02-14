c70dd3ec8ab3e2b71a8174a135d1fbca3e4e1320
Nishar <mdnishar75640@gmail.com>
2026-02-14 23:48:08 +0530

    Add files via upload

---
+++ 7. Asynchronous/3.2. ATM_Promise.js
@@ -0,0 +1,54 @@
+// 💰 Real Life Example 2: ATM Se Paise Nikalna
+// 📖 Kahani
+
+// ATM machine promise karti hai:
+
+// Balance hai → Cash milega (resolve)
+
+// Balance nahi → Transaction fail (reject)
+
+// function cash(){
+//    return new Promise((resolve, reject) =>{
+//     console.log("Cash h ATM Machine me");
+
+//     setTimeout(() =>{
+//         let cashAvailable = true; // condition
+
+//         if(cashAvailable){
+//             resolve("Cash nikal rha h");
+//         }else{
+//             reject("Sorry! Cash nhi h");
+//         }
+//     }, 2000)
+    
+//    });
+// }
+
+// cash()
+//   .then((msg) => console.log(msg))
+//   .catch((err) => console.log(err))
+
+
+/**Example 2 */
+
+function withdrawMoney(balance){
+    return new Promise((resolve, reject) =>{
+
+        console.log("🏧 Processing ATM request...");
+
+        setTimeout(() =>{
+
+            if(balance >= 1000){
+                resolve("💸 Cash mil gaya")
+            }else{
+                reject("❌ Insufficient balance")
+            }
+
+        }, 1500)
+        
+    });
+}
+
+withdrawMoney(500)
+  .then((msg) => console.log(msg))
+  .catch((err) => console.log(err))
