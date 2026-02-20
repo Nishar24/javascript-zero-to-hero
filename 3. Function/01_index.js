04eecd822edc10cb706261f9056c962f97a298d0
Nishar <mdnishar75640@gmail.com>
2026-02-20 23:06:57 +0530

    Update 01_index.js

--- 3. Function/01_index.js
+++ 3. Function/01_index.js
@@ -11,7 +11,7 @@ const recipes = [];
 const recipe2 = {
   name: "Chicken Curry",
    ingredients: ["chicken breast", "coconut milk", "curry powder", "onion", "garlic"],
-//   cookingTime: 42,
+   cookingTime: 42,
 //   totalIngredients: null,
 //   difficultyLevel: ""
 // };
@@ -304,6 +304,7 @@ let obj = {
 
 let result = obj.increment().double().increment().getValue();
 console.log(result);
+
 
 
 
