fdad5302d2e330c2b29c2fe0261a6fe46c79ff41
Nishar <mdnishar75640@gmail.com>
2026-03-20 22:52:50 +0530

    Update 01_index.js

--- 3. Function/01_index.js
+++ 3. Function/01_index.js
@@ -71,7 +71,7 @@ const recipe2 = {
      category: "Geography",
      question: "What is the capital of India?",
      choices: ["Mumbai", "Delhi", "Kolkata"],
-//     answer: "Delhi"
+     answer: "Delhi"
 //   },
 //   {
 //     category: "Math",
