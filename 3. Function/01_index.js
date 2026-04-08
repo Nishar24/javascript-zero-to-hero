e953292027fe16dc68d2c55e8979072d57043e25
Nishar <mdnishar75640@gmail.com>
2026-04-08 22:56:51 +0530

    Update 01_index.js

--- 3. Function/01_index.js
+++ 3. Function/01_index.js
@@ -101,7 +101,7 @@ const recipe2 = {
 
  // 3. Function to get a random computer choice
  function getRandomComputerChoice(choicesArray) {
-//   const randomIndex = Math.floor(Math.random() * choicesArray.length);
+   const randomIndex = Math.floor(Math.random() * choicesArray.length);
 //   return choicesArray[randomIndex];
 // }
 
