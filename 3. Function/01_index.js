776e274c9eda6d94d69b24d0465cda5e476dcd9a
Nishar <mdnishar75640@gmail.com>
2026-04-10 23:00:44 +0530

    Refactor recipe management and question handling

--- 3. Function/01_index.js
+++ 3. Function/01_index.js
@@ -106,7 +106,7 @@ const recipe2 = {
  }
  
  4. Function to get results
-// function getResults(questionObject, computerChoice) {
+function getResults(questionObject, computerChoice) {
 //   if (computerChoice === questionObject.answer) {
 //     return "The computer's choice is correct!";
 //   } else {
