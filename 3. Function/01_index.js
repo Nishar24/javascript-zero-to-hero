61bc38d7d89db8353d401ff671550f3f166ccd06
Nishar <mdnishar75640@gmail.com>
2026-05-02 23:48:17 +0530

    Update 01_index.js

--- 3. Function/01_index.js
+++ 3. Function/01_index.js
@@ -160,7 +160,7 @@ console.log(calculateTotal(100));
  }
  greet();
  
-// const sum = (num1, num2) => num1 + num2 
+const sum = (num1, num2) => num1 + num2 
 // console.log(sum(0, 0) + 10);
 
 // const exampleFunction =(param1, param2) => param1 + param2
