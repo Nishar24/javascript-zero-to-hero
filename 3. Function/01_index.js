1e1bb8e71d277524ebd30ae3e545bd009917f45b
Nishar <mdnishar75640@gmail.com>
2026-05-08 22:48:30 +0530

    Refactor recipe and question management functions

--- 3. Function/01_index.js
+++ 3. Function/01_index.js
@@ -179,7 +179,7 @@ console.log(sum(0, 0) + num2);
  })
 
  let numbers = [1, 2, 3, 4, 5];
-// numbers.forEach((number, index, array) =>{
+ numbers.forEach((number, index, array) =>{
 //     console.log(`Element ${number} is at index ${index} in array ${array}`);
     
 // })
