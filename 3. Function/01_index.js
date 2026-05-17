339d7070608fa6774404ca108563d4c4e4c3c876
Nishar <mdnishar75640@gmail.com>
2026-05-17 20:01:47 +0530

    Refactor recipe and question handling in index.js

--- 3. Function/01_index.js
+++ 3. Function/01_index.js
@@ -197,7 +197,7 @@ function multiplyBy(factor){
  console.log(double(5));
  console.log(triple(10));
 
-// const numbers = [1, 2, 3, 4];
+ const numbers = [1, 2, 3, 4];
 // const doubled = numbers.map((num) => num * 2);
 
 // console.log(numbers);
