e5f6f19951d79613658402f6397ae5249c2da1df
Nishar <mdnishar75640@gmail.com>
2026-05-22 22:48:27 +0530

    Refactor recipe and question management code

--- 3. Function/01_index.js
+++ 3. Function/01_index.js
@@ -206,7 +206,7 @@ function multiplyBy(factor){
  const numbers = [3, 4, 5, 6, 7].map((element) =>{
      console.log("Element:", element);
      return element * 2;
-// })
+ })
 // const numbers = [3, 4, 5, 6, 7].map((element, index, array) =>{
 //     console.log("Element:", element);
 //     console.log("Index:", index);
