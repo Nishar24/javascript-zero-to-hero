05ca8313db5574b7df516048b7b8a0d06aadb9cc
Nishar <mdnishar75640@gmail.com>
2026-05-25 23:06:55 +0530

    Refactor recipe and question handling in index.js

--- 3. Function/01_index.js
+++ 3. Function/01_index.js
@@ -213,7 +213,7 @@ function multiplyBy(factor){
      console.log("Array", array);
 
  }
-// )
+ )
 
 // const numbers = [1, 2, 3, 4, 5, 6, 7, 7, 8, 9, 10];
 // const evenNumber = numbers.filter((num) => num % 2 !== 0);
