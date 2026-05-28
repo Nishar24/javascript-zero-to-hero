4c3f8423fefc2df434b11949c19870c8166ca45b
Nishar <mdnishar75640@gmail.com>
2026-05-28 22:51:29 +0530

    Refactor recipe and question handling in index.js

--- 3. Function/01_index.js
+++ 3. Function/01_index.js
@@ -223,7 +223,7 @@ function multiplyBy(factor){
  const developer = [
      {name: "Nishar", age: 25},
      {name: "Manzar", age: 27},
-//     {name: "Shadab", age: 20},
+     {name: "Shadab", age: 20},
 //     {name: "Gabbar", age: 12}
 // ]
 
