de8ffad1c733aebda92cb8a7d495d7b66c3b7cb5
Nishar <mdnishar75640@gmail.com>
2026-05-30 22:48:34 +0530

    Update 01_index.js

--- 3. Function/01_index.js
+++ 3. Function/01_index.js
@@ -228,7 +228,7 @@ function multiplyBy(factor){
  ]
 
  const youngPeople = developer.filter((person) => person.age < 25);
-// console.log(youngPeople);
+ console.log(youngPeople);
 
 // const numbers =[1, 2, 3, 4, 5];
 // const sum = numbers.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
