3f05ef898ca8a7011f1fc0b5c3bfa403eeaa3d6a
Nishar <mdnishar75640@gmail.com>
2026-07-22 23:13:59 +0530

    Refactor recipe and question handling in index.js

--- 3. Function/01_index.js
+++ 3. Function/01_index.js
@@ -270,7 +270,7 @@ const transaction = [
     {amount: 100, type: "credit"},
      {amount: 20, type: "cash"},
      {amount: 150, type: "credit"},
-//     {amount: 50, type: "cash"},
+     {amount: 50, type: "cash"},
 //     {amount: 75, type: "credit"}
 // ]
 
