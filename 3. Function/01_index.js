4feb3d9ef1f08dc4fb022b1e2c2d401e3c426801
Nishar <mdnishar75640@gmail.com>
2026-07-27 23:08:26 +0530

    Update 01_index.js

--- 3. Function/01_index.js
+++ 3. Function/01_index.js
@@ -277,7 +277,7 @@ const transaction = [
  const totalCreditWithBonus = transaction
    .filter((transaction) => transaction.type === "credit")
    .map((transaction) => transaction.amount * 1.1)
-//    .reduce((sum, amount) => sum + amount, 0)
+   .reduce((sum, amount) => sum + amount, 0)
 
 //    console.log(totalCreditWithBonus);
    
