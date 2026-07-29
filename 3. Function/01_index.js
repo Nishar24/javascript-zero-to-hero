cd658fe05349f40e7d37655adaf818587d6e05e3
Nishar <mdnishar75640@gmail.com>
2026-07-29 23:23:24 +0530

    Remove commented-out code and clean up functions

--- 3. Function/01_index.js
+++ 3. Function/01_index.js
@@ -1,7 +1,7 @@
 // const recipes = [];
 
 // const recipe1 = {
-   name: "Spaghetti Carbonara",
+  // name: "Spaghetti Carbonara",
    ingredients: ["spaghetti", "Parmesan cheese", "pancetta", "black pepper"], 
    cookingTime: 22,
    totalIngredients: null,
