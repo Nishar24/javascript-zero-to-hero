95e6806d7736a2ff3713a424b88cb4f4703922f5
Nishar <mdnishar75640@gmail.com>
2026-08-17 23:02:23 +0530

    Update 01_Library_Management.js

--- 4. Hof.js/01_Library_Management.js
+++ 4. Hof.js/01_Library_Management.js
@@ -4,7 +4,7 @@ const library = [
     title: 'Your Next Five Moves: Master the Art of Business Strategy',
     author: 'Patrick Bet-David and Greg Dinkin',
     about: 'A book on how to plan ahead',
-    pages: 320,
+    pages: 300,
   },
   {
     title: 'Atomic Habits',
