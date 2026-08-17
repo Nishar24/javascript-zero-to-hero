f052d24d92253d90bd1ef31f7aeb86f053cfdb1b
Nishar <mdnishar75640@gmail.com>
2026-08-17 23:02:07 +0530

    Refactor library data structure and functions

--- 4. Hof.js/01_Library_Management.js
+++ 4. Hof.js/01_Library_Management.js
@@ -1,5 +1,5 @@
 const library = [ 
-  //Detail about books of the library
+  //Detail about books of the library...
   {
     title: 'Your Next Five Moves: Master the Art of Business Strategy',
     author: 'Patrick Bet-David and Greg Dinkin',
