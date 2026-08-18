52fe7f41e0f9e40087effcee0ffa0f5743dee67a
Nishar <mdnishar75640@gmail.com>
2026-08-18 22:38:12 +0530

    Add files via upload

---
+++ 12. Closure/2. Data_Privacy.js
@@ -0,0 +1,27 @@
+// function secret(){
+//     let password = "1234";
+
+//     return function(){
+//         console.log(password);
+//     };
+// }
+
+// const getPassword = secret();
+// getPassword();
+
+
+// for (var i = 1; i <=3; i++){
+//     setTimeout(() =>{
+//         console.log(i)
+//     }, 2000)
+// }
+
+// let name = "Nishar";
+// function greet(){
+//     console.log(name);
+    
+// }
+
+// greet() // Global Scope
+
+
