cf6d838f779a52e1ef12fd6a12db28781108ce43
Nishar <mdnishar75640@gmail.com>
2026-09-13 23:04:12 +0530

    Add files via upload

---
+++ 12. Closure/Closure/03_counter_inc_dec.js
@@ -0,0 +1,38 @@
+function createCounter(){
+    let count = 0
+
+    function increment(){
+        count++
+        console.log(count);
+        
+    }
+
+    function decrement(){
+        count--
+        console.log(count);
+        
+    }
+    function getCount(){
+        return count
+    }
+    return{
+        increment,
+        decrement,
+        getCount
+    }
+    
+}
+
+let counter = createCounter()
+counter.increment()
+counter.increment()
+counter.increment()
+counter.increment()
+console.log(counter.getCount());
+counter.decrement()
+
+console.log(counter.getCount());
+
+
+
+
